7bd4df7a35e38a9b785c29b09727acff6c5924ad
Ethan Newlin <newlin5@gmail.com>
2023-09-27 23:05:15 -0400

    updated to new platform

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,51 +1,48 @@
-import axios from 'axios';
-const auth = (state = { }, action)=> {
-  if(action.type === 'SET_AUTH'){
-    return action.auth;
-  }
-  return state;
+import axios from "axios";
+const auth = (state = {}, action) => {
+	if (action.type === "SET_AUTH") {
+		return action.auth;
+	}
+	return state;
 };
 
-export const logout = ()=> {
-  window.localStorage.removeItem('token');
-  return { type: 'SET_AUTH', auth: {} };
+export const logout = () => {
+	window.localStorage.removeItem("token");
+	return { type: "SET_AUTH", auth: {} };
 };
 
-
-export const loginWithToken = ()=> {
-  return async(dispatch)=> {
-    const token = window.localStorage.getItem('token');
-    if(token) {
-      const response = await axios.get('/api/auth', {
-        headers: {
-          authorization: token
-        }
-      });
-      dispatch({ type: 'SET_AUTH', auth: response.data });
-    }
-    
-  };
+export const loginWithToken = () => {
+	return async (dispatch) => {
+		const token = window.localStorage.getItem("token");
+		if (token) {
+			const response = await axios.get("/api/auth", {
+				headers: {
+					authorization: token,
+				},
+			});
+			dispatch({ type: "SET_AUTH", auth: response.data });
+		}
+	};
 };
 
-export const updateAuth = (auth)=> {
-  return async(dispatch)=> {
-    const token = window.localStorage.getItem('token');
-    const response = await axios.put('/api/auth', auth, {
-      headers: {
-        authorization: token
-      }
-    });
-    dispatch({ type: 'SET_AUTH', auth: response.data });
-  };
+export const updateAuth = (auth) => {
+	return async (dispatch) => {
+		const token = window.localStorage.getItem("token");
+		const response = await axios.put("/api/auth", auth, {
+			headers: {
+				authorization: token,
+			},
+		});
+		dispatch({ type: "SET_AUTH", auth: response.data });
+	};
 };
 
-
-export const attemptLogin = (credentials)=> {
-  return async(dispatch)=> {
-    const response = await axios.post('/api/auth', credentials);
-    window.localStorage.setItem('token', response.data);
-    dispatch(loginWithToken());
-  };
+export const attemptLogin = (credentials) => {
+	return async (dispatch) => {
+		const response = await axios.post("/api/auth", credentials);
+		window.localStorage.setItem("token", response.data);
+		dispatch(loginWithToken());
+	};
 };
 
 export default auth;
